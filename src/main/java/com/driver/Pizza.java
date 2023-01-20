c6f17cb7d80f3ee3fe20c3411b5b82799a170d18
Mohammad-Mubarak <beyourbestversion3@gmail.com>
2023-01-20 16:55:10 +0530

    done  3

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,10 +1,10 @@
 package com.driver;
 
 public class Pizza {
-    int Non_veg=400;
-    int Veg=300;
-    int Extra_Cheese=80;
-    int Extra_Topping=70;
+  private int Non_veg=400;
+  private  int Veg=300;
+  private  int Extra_Cheese=80;
+    private int Extra_Topping=70;
     int Extra_Tro_nonVeg=120;
     int Pager_Bag=0;
     private int price;
@@ -15,25 +15,21 @@ public class Pizza {
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         if(isVeg){
-            this.bill="Base Price Of The Pizza: 300 \n";
+            this.bill="Base Price Of The Pizza: 300\n";
             this.price+=Veg;
         }else{
-            this.bill="Base Price Of The Pizza: 400 \n";
+            this.bill="Base Price Of The Pizza: 400\n";
             this.price+=Non_veg;
         }
     }
-
     public int getPrice(){
       return this.price;
     }
-
     public void addExtraCheese(){
         if(asked_top==false){
             this.price+=Extra_Cheese;
             asked_top=true;
         }
-
-
     }
 
     public void addExtraToppings(){
@@ -52,19 +48,18 @@ public class Pizza {
 
     public void addTakeaway(){
         // your code goes here
-        Pager_Bag+=20;
-        this.price+=Pager_Bag;
+        this.price+=20;
     }
 
     public String getBill(){
         if(asked_top==true){
-            this.bill+="Extra Cheese Added: 80 \n";
+            this.bill+="Extra Cheese Added: 80\n";
         }
         if(asked==true){
             if(isVeg==true){
-                this.bill+="Extra Toppings Added: 70 \n";
+                this.bill+="Extra Toppings Added: 70\n";
             }else{
-                this.bill+="Extra Toppings Added: 120 \n";
+                this.bill+="Extra Toppings Added: 120\n";
             }
         }
         if(Pager_Bag>20){
